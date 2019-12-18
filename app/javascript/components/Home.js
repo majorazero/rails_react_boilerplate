35cd702d70cd00afb19201b26f095534b87ebecc
Dan Hsu <daniel.cw.hsu@gmail.com>
2019-12-17 20:44:42 -0800

    introduced basic starter components

---
+++ app/javascript/components/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    console.log('!');
+    return (
+      <div>
+        This is my boilerplate homepage!
+      </div>
+    );
+  }
+}
+
+export default Home;
+
